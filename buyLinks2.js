61714276e1278da985a7136da641b20b93dc1398
David Davies <d_davies@apple.com>
2019-10-14 20:23:41 -0700

    Updated to new version of jquery

--- buyLinks2.js
+++ buyLinks2.js
@@ -1,13 +1,13 @@
 javascript: (function(){
 
     // the minimum version of jQuery we want
-    var v = "1.3.2";
+    var v = "3.4.1";
 
     // check prior inclusion and version
     if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
         var done = false;
         var script = document.createElement("script");
-        script.src = "jquery.min.js";
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
         script.onload = script.onreadystatechange = function(){
             if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
                 done = true;
