4301f16e855161568e2067689a4e5c54f14a19f2
David Davies <d_davies@apple.com>
2019-10-15 21:49:35 -0700

    Jquery download source referenced for code

--- buyLinks2.js
+++ buyLinks2.js
@@ -7,7 +7,7 @@ javascript: (function(){
     if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
         var done = false;
         var script = document.createElement("script");
-        script.src = "jquery.min.js";
+        script.src = "https://uhpotato.github.io/buyLinks/jquery.min.js";
         script.onload = script.onreadystatechange = function(){
             if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
                 done = true;
